099927ac18b15f8fb966a0dee79fc16de6611cc1
Andrzej Amghar <andrzejamghar@gmail.com>
2015-12-05 20:15:27 +0100

    Changed abbreviation from [...] to (...)

--- wp_theme/skntelephoners/functions.php
+++ wp_theme/skntelephoners/functions.php
@@ -1,8 +1,12 @@
-<?php
+﻿<?php
 
 function register_sknmenu() {
   register_nav_menu('primary-menu',__( 'Główne menu strony' ));
 }
 add_action( 'init', 'register_sknmenu' );
 
+function new_excerpt_more( $more ) {
+	return '(...)';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
 ?>
